9d03b03e3fabc442a90492a9192d9dd45ef8ca05
christianofernandes3108-cloud <christianofernandes3108@gmail.com>
2026-03-02 18:09:07 +0200

    Update frontend js

--- client/js/api.js
+++ client/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://ledgerpro-fullstack.onrender.com/api";
 
 // Get token from localStorage
 function getToken() {
--- client/js/auth.js
+++ client/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://ledgerpro-fullstack.onrender.com/api";
 
 function setLoading(button, isLoading, text) {
   if (isLoading) {
